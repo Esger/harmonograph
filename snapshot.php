a1ed3c07a1cccba848f8a5dd856d35f3cc90e354
Esger Jellema <esgerj@gmail.com>
2018-11-17 21:00:43 +0100

    fix https errors and remove save function
    broken in php 7

--- snapshot.php
+++ snapshot.php
@@ -94,15 +94,15 @@ if (isset($GLOBALS["HTTP_RAW_POST_DATA"]))
 
 echo '
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="https://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta name="description" content="Interactive Harmonograph - Create your own artwork!">
-	<meta property="og:url" content="http://www.ashware.nl/harmonograph/snapshot.php?imageId='.$imageId.'" />
+	<meta property="og:url" content="https://www.ashware.nl/harmonograph/snapshot.php?imageId='.$imageId.'" />
 	<meta property="og:title" content="This is my creation... what\'s yours?"/>
-	<meta property="og:image" content="http://www.ashware.nl/harmonograph/img/harmonogram-'.$imageId.'.jpg" />
+	<meta property="og:image" content="https://www.ashware.nl/harmonograph/img/harmonogram-'.$imageId.'.jpg" />
 	<meta property="og:description" content="Interactive Harmonograph - Create your own artwork!" />
-	<link rel="image_src" href="http://www.ashware.nl/harmonograph/img/harmonogram-'.$imageId.'.jpg" />
+	<link rel="image_src" href="https://www.ashware.nl/harmonograph/img/harmonogram-'.$imageId.'.jpg" />
 	<title>Create your own harmonogram and share it...</title>
 	<style>
 		body{
@@ -126,18 +126,18 @@ echo '
 			left:0; top:0;
 		}
 	</style>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
 </head>
 
 <body>
-	<h1><a href="http://www.ashware.nl/harmonograph">Create your own harmonogram here</a></h1>
+	<h1><a href="https://www.ashware.nl/harmonograph">Create your own harmonogram here</a></h1>
 	<img class="harmonogram" src="img/harmonogram-'.$imageId.'.jpg" alt="mijn harmonogram" />
 	<div class="addThis">
 		<!-- AddThis Button BEGIN -->
 		<div class="addthis_toolbox addthis_default_style ">
 			<a class="addthis_counter addthis_pill_style"></a>
 		</div>
-		<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4f3c4b976815089f"></script>
+		<script type="text/javascript" src="https://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4f3c4b976815089f"></script>
 		<!-- AddThis Button END -->
 	</div>
 	
